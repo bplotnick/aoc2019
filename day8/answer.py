7610e7cd99052545a296fb1247fd84fe7f61f534
Ben Plotnick <ben.plotnick@getcruise.com>
2019-12-11 01:07:59 -0800

    day8 part2

--- day8/answer.py
+++ day8/answer.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python3
 import numpy as np
 
+# Part 1
 shape = (-1, 6, 25)
-#shape = (-1, 2, 3)
 with open('input.txt') as f:
     vals = f.read().strip()
-#vals = "123456789012"
 a = np.array([int(x) for x in vals])
 a = np.reshape(a, shape)
 minlayer = np.argmin(np.sum(a==0, axis=(1,2)))
 b = a[minlayer,:,:]
 print(np.sum(b==1)*np.sum(b==2))
+
+# Part 2
+result = np.full_like(a[0,:,:], 2)
+# Can probably use ufuncs to do this loop entirely in C
+for i in range(a.shape[0]):
+    layer = a[i,:,:]
+    result = np.where(np.logical_and(result == 2, layer != 2), layer, result)
+print(result)
